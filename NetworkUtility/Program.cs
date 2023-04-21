413f205ac0b4f1920c364e469c00e68b8ed9e877
joshuamainsmith <joshuamainsmith@gmail.com>
2023-04-21 11:12:55 -0700

    abstracted ping service in program

--- NetworkUtility/Program.cs
+++ NetworkUtility/Program.cs
@@ -1,37 +1,6 @@
 ﻿using NetworkUtility.Services;
-using System;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Text;
 
-namespace Examples.System.Net.NetworkInformation.PingTest
-{
-    public class PingExample
-    {
-        public static void Main()
-        {
-            PingService pingService = new PingService();
-            Ping pingSender = new Ping();
-            PingOptions options = new PingOptions();
-
-            // Use the default Ttl value which is 128,
-            // but change the fragmentation behavior.
-            options.DontFragment = true;
-
-            // Create a buffer of 32 bytes of data to be transmitted.
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 120;
-            string address = "google.com";
-            PingReply reply = pingSender.Send(address, timeout, buffer, options);
-            if (reply.Status == IPStatus.Success)
-            {
-                Console.WriteLine("Address: {0}", reply.Address.ToString());
-                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
-                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
-                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
-                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
-            }
-        }
-    }
-}
+PingService pingService = new PingService();
+pingService.ReadPingInfo(
+    pingService.SendPing("google.com")
+    );
