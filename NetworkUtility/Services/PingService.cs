13c006fca41829d99d67aa59f8dfaaab93a83bf7
joshuamainsmith <joshuamainsmith@gmail.com>
2023-04-21 11:12:43 -0700

    Single address send ping and read ping created

--- NetworkUtility/Services/PingService.cs
+++ NetworkUtility/Services/PingService.cs
@@ -9,18 +9,45 @@ namespace NetworkUtility.Services
 {
     internal class PingService
     {
-        public Ping pingSender { get; set; }
-        public PingOptions pingOptions { get; set; }
+        Ping pingSender { get; set; }
+        PingOptions pingOptions { get; set; }
         PingReply pingReply { get; set; }
-        public string data { get; set; }
-        public byte[] buffer { get; set; }
-        public int timeout { get; set; }
-        public string address { get; set; }
+        string data { get; set; }
+        byte[] buffer { get; set; }
+        int timeout { get; set; }
+        string address { get; set; }
 
         public PingService()
         {
             pingSender = new Ping();
             pingOptions = new PingOptions();
+            pingOptions.DontFragment = true;
+            data = "**Network Connection Test Ping**";
+            buffer = Encoding.ASCII.GetBytes(data);
+            timeout = 120;
+            address = "localhost";
+        }
+
+        public PingReply SendPing(string address)
+        {
+            this.address = address;
+
+            pingReply = pingSender.Send(address, timeout, buffer, pingOptions);
+
+            return pingReply;
+        }    
+
+        public void ReadPingInfo(PingReply pingReply)
+        {
+            Console.WriteLine(pingReply.Status);
+            if (pingReply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", pingReply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", pingReply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", pingReply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", pingReply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", pingReply.Buffer.Length);
+            }
         }
     }
 }
