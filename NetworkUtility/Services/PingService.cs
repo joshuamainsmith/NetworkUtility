9812cd6d14e5c87bcbcb73eec6a2f6821350b878
joshuamainsmith <joshuamainsmith@gmail.com>
2023-04-20 22:05:54 -0700

    Add project files.

---
+++ NetworkUtility/Services/PingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Services
+{
+    internal class PingService
+    {
+        public Ping pingSender { get; set; }
+        public PingOptions pingOptions { get; set; }
+        PingReply pingReply { get; set; }
+        public string data { get; set; }
+        public byte[] buffer { get; set; }
+        public int timeout { get; set; }
+        public string address { get; set; }
+
+        public PingService()
+        {
+            pingSender = new Ping();
+            pingOptions = new PingOptions();
+        }
+    }
+}
