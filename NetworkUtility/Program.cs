114f8a3352cf5eafadd3d310ee58e9ecaea9307f
joshuamainsmith <joshuamainsmith@gmail.com>
2023-04-24 06:35:09 -0700

    Added test code

--- NetworkUtility/Program.cs
+++ NetworkUtility/Program.cs
@@ -8,6 +8,8 @@ pingService.SendPing("https://www.google.com");
 
 pingService.SendPing("1234");
 
+pingService.SendPing("1234.doesnotexist");
+
 pingService.SendPing("google.com", "bing.com");
 
 pingService.SendPingByFile("C:\\addresses.txt");
