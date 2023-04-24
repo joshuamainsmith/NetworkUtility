78c8e09102208e72d52ad0aa6b22aef2ec24ac69
joshuamainsmith <joshuamainsmith@gmail.com>
2023-04-24 16:30:55 -0700

    Added unit test for sing addess input

---
+++ NetworkUtility.Tests/PingTests/PingServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NetworkUtility.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class PingServiceTests
+    {
+        // ClassName_MethodName_ReturnType()
+        [Fact]
+        public void GivenSingleAddress_SendPing_ReturnsPingReply()
+        {
+            // Arrange - variables, classes, mocks
+            var pingService = new PingService();
+
+            // Act - Call action methods
+            var result = pingService.SendPing("google.com");
+
+            // Assert - compare expected return vals with actual
+            pingService.Should().NotBeNull();
+            result.Should().NotBeNull();
+            result.Address.ToString().Should().NotBeNullOrWhiteSpace();
+            result.Options.Ttl.Should().BeGreaterThanOrEqualTo(0);
+            result.RoundtripTime.Should().BeGreaterThan(0);
+            result.Options.DontFragment.Should().Be(false);
+            result.Should().BeOfType<PingReply>().Which.Buffer.Length.Should().BeLessThanOrEqualTo(65507);   // https://stackoverflow.com/q/9449837
+        }
+    }
+}
