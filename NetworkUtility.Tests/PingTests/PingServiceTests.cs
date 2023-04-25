5e0a78278efd2ad5e28bdb79823c3e5ecb50bfc5
joshuamainsmith <joshuamainsmith@gmail.com>
2023-04-25 15:22:58 -0700

    Consolidated and added some more tests

--- NetworkUtility.Tests/PingTests/PingServiceTests.cs
+++ NetworkUtility.Tests/PingTests/PingServiceTests.cs
@@ -11,17 +11,21 @@ using System.Threading.Tasks;
 
 namespace NetworkUtility.Tests.PingTests
 {
-    public class PingServiceTests
+    public class PingServiceTests : PingService
     {
         // ClassName_MethodName_ReturnType()
-        [Fact]
-        public void GivenSingleHost_SendPing_ReturnsPingReply()
+        [Theory]
+        [InlineData("google.com")]
+        [InlineData("127.0.0.1")]
+        [InlineData("google.com", "bing.com", "127.0.0.1", "142.251.211.238")]
+        [InlineData("learn.microsoft.com", "about.google.com", "www.google.com", "console.cloud.google.com")]
+        public void PingService_SendPing_ReturnsPingReply(params string[] addresses)
         {
             // Arrange - variables, classes, mocks
             var pingService = new PingService();
 
             // Act - Call action methods
-            var result = pingService.SendPing("google.com");
+            var result = pingService.SendPing(addresses);
 
             // Assert - compare expected return vals with actual
             pingService.Should().NotBeNull();
@@ -31,33 +35,22 @@ namespace NetworkUtility.Tests.PingTests
             result.RoundtripTime.Should().BeGreaterThan(0);
             result.Options.DontFragment.Should().Be(false);
             result.Should().BeOfType<PingReply>().Which.Buffer.Length.Should().BeLessThanOrEqualTo(65507);   // https://stackoverflow.com/q/9449837
-        }
+        }        
 
-        [Fact]
-        public void GivenMultipleAddresses_SendPing_ReturnsPingReply()
+        [Theory]
+        [InlineData("1234")]
+        [InlineData("1234.doesnotexist")]
+        [InlineData("!@#$%^&.url.com")]
+        [InlineData("console.cloud.google.com/getting-started")]
+        [InlineData("https://www.google.com", "!@#$%^&.url.com", "console.cloud.google.com/getting-started")]
+        public void PingService_SendPing_ReturnsNull(params string[] addresses)
         {
             var pingService = new PingService();
-
-            var result = pingService.SendPing("google.com", "bing.com", "127.0.0.1", "142.251.211.238");
-
-            pingService.Should().NotBeNull();
-            result.Should().NotBeNull();
-            result.Address.ToString().Should().NotBeNullOrWhiteSpace();
-            result.Options.Ttl.Should().BeGreaterThanOrEqualTo(0);
-            result.RoundtripTime.Should().BeGreaterThan(0);
-            result.Options.DontFragment.Should().Be(false);
-            result.Should().BeOfType<PingReply>().Which.Buffer.Length.Should().BeLessThanOrEqualTo(65507);   // https://stackoverflow.com/q/9449837
-        }
-
-        [Fact]
-        public void GivenSingleHostBadInput_SendPing_ReturnsPingReply()
-        {
-            var pingService = new PingService();
-
-            var result = pingService.SendPing("1234");
-
+            
+            var result = pingService.SendPing(addresses);
+            
             pingService.Should().NotBeNull();
             result.Should().BeNull();
-        }
+        }        
     }
 }
