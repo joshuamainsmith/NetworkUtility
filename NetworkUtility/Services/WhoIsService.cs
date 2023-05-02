19aca9d03acce9a785ac93039512fbd788fbf437
joshuamainsmith <joshuamainsmith@gmail.com>
2023-05-01 17:15:29 -0700

    Added WhoIs Service

---
+++ NetworkUtility/Services/WhoIsService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Whois.NET;
+
+namespace NetworkUtility.Services
+{
+    public class WhoIsService
+    {
+        WhoisResponse? response { get; set; }
+        string? host { get; set; }
+
+        public WhoIsService()
+        {
+            response = null;
+            host = null;
+        }
+
+        public WhoIsService(string host)
+        {
+            response = null;
+            this.host = host;
+        }
+
+        public WhoisResponse? WhoIs()
+        {
+            if (host == null) return null;
+
+            response = WhoisClient.Query(host);
+
+            return response;
+        }
+
+        public WhoisResponse? WhoIs(string host)
+        {
+            this.host = host;
+
+            response = WhoIs();
+
+            return response;
+        }
+    }
+}
