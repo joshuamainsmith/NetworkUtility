4e04630b39ccbe17f7701562845c98de9a5a2067
joshuamainsmith <joshuamainsmith@gmail.com>
2023-05-02 11:55:06 -0700

    Added output from whois query

--- NetworkUtility/Services/WhoIsService.cs
+++ NetworkUtility/Services/WhoIsService.cs
@@ -9,6 +9,11 @@ namespace NetworkUtility.Services
 {
     public class WhoIsService
     {
+        private const string OrganizationName = "Organization Name";
+        private const string AddressRange = "Address Range";
+        private const string RespondedServers = "Responded Servers";
+        private const string RawData = "Raw Data";
+
         WhoisResponse? response { get; set; }
         string? host { get; set; }
 
@@ -40,6 +45,77 @@ namespace NetworkUtility.Services
             response = WhoIs();
 
             return response;
+        }
+
+        public void GetWhoIsInfo(bool OrganizationName = false, bool AddressRange = false, 
+            bool RespondedServers = false, bool RawData = true)
+        {
+            if (response== null) return;
+            
+            if (OrganizationName && (response.OrganizationName != null))
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write(WhoIsService.OrganizationName);
+
+                Console.ResetColor();
+
+                Console.WriteLine();
+
+                Console.WriteLine(response.OrganizationName);
+
+                Console.WriteLine();
+            }
+            if (AddressRange && (response.AddressRange != null))
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write(WhoIsService.AddressRange);
+
+                Console.ResetColor();
+
+                Console.WriteLine();
+
+                Console.WriteLine("{0} - {1}", response.AddressRange.Begin, response.AddressRange.End);
+
+                Console.WriteLine();
+            }
+            if (RespondedServers && (response.RespondedServers != null))
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write(WhoIsService.RespondedServers);
+
+                Console.ResetColor();
+
+                Console.WriteLine();
+
+                foreach (var server in response.RespondedServers)
+                {
+                    Console.WriteLine(server);
+                }
+
+                Console.WriteLine();
+            }
+            if (RawData && (response.Raw != null))
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Write(WhoIsService.RawData);
+
+                Console.ResetColor();
+
+                Console.WriteLine();
+
+                Console.WriteLine(response.Raw);
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
