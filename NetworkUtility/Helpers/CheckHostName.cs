2cc634ce30b2fade86988140aa4c3caf36bd951d
joshuamainsmith <joshuamainsmith@gmail.com>
2023-05-02 16:53:21 -0700

    Updated regex (was getting false negative)

--- NetworkUtility/Helpers/CheckHostName.cs
+++ NetworkUtility/Helpers/CheckHostName.cs
@@ -9,8 +9,8 @@ namespace NetworkUtility.Helpers
 {
     public class CheckHostName
     {
-        // https://regexper.com/#%5E%28www%5C.%29%3F%5Cw%2B%28%5C.%5Cw%2B%29%2B%24
-        static Regex checkValidHostOrIP = new Regex(@"^(www\.)?\w+(\.\w+)+$",
+        // https://regexper.com/#%28www%5C.%29%3F%5Cw%2B%28%5C.%5Cw%2B%29%2B
+        static Regex checkValidHostOrIP = new Regex(@"(www\.)?\w+(\.\w+)+",
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public bool CheckHostNameOrAddress(string address)
