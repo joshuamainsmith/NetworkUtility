7c7eef1b5c198f6362dd3ebc09c2e213e534ab8a
joshuamainsmith <joshuamainsmith@gmail.com>
2023-05-08 16:54:53 -0700

    Working on regex

--- NetworkUtility/Helpers/CheckHostName.cs
+++ NetworkUtility/Helpers/CheckHostName.cs
@@ -10,7 +10,7 @@ namespace NetworkUtility.Helpers
     public class CheckHostName
     {
         // https://regexper.com/#%28www%5C.%29%3F%5Cw%2B%28%5C.%5Cw%2B%29%2B
-        static Regex checkValidHostOrIP = new Regex(@"(www\.)?\w+(\.\w+)+",
+        static Regex checkValidHostOrIP = new Regex(@"[^\W](www\.)?\w+(\.\w+)+",
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public bool CheckHostNameOrAddress(string address)
