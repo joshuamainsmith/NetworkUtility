24248404e253e1a41b98cc349f63f037f3f33301
joshuamainsmith <joshuamainsmith@gmail.com>
2023-05-09 16:15:41 -0700

    Starting export service

---
+++ NetworkUtility/Services/ExportService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Services
+{
+    public class ExportService
+    {
+        string path { get; set; }
+        StreamWriter writeText { get; set; }
+        StringBuilder buffer { get; set; }
+
+        public ExportService()
+        {
+            path = string.Empty;
+            buffer = new StringBuilder();
+        }
+
+        public bool writeOutput(string path, string buffer)
+        {
+            this.path = path;
+            this.buffer = new StringBuilder(buffer);
+            writeText = File.AppendText(path);
+            writeText.WriteLine(buffer);
+            writeText.Flush();
+
+            return false;
+        }
+    }
+}
