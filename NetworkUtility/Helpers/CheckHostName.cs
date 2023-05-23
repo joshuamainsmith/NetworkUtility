16ca40d3fcc4d5de1bd47ed9048866eb62309b47
joshuamainsmith <joshuamainsmith@gmail.com>
2023-05-23 16:36:35 -0700

    Made it better

--- NetworkUtility/Helpers/CheckHostName.cs
+++ NetworkUtility/Helpers/CheckHostName.cs
@@ -9,8 +9,8 @@ namespace NetworkUtility.Helpers
 {
     public class CheckHostName
     {
-        // https://regexper.com/#%5E%28www%5C.%29%3F%28%5C-%3F%5Cw%29%2B%28%5C.%5Cw%2B%29%2B%24
-        static Regex checkValidHostOrIP = new Regex(@"^(www\.)?(\-?\w)+(\.\w+)+$",
+        // https://regexper.com/#%5E%28www%5C.%29%3F%28%5Cw%28%5C-%3F%5Cw%29%3F%29%2B%28%28%5C.%5Cw%2B%29%3F%29%2B%24
+        static Regex checkValidHostOrIP = new Regex(@"^(www\.)?(\w(\-?\w)?)+((\.\w+)?)+$",
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public bool CheckHostNameOrAddress(string address)
